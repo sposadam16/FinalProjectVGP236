abf6b35b836b7f9c8b7b911d51117532af38cab8
Matuzz1206 <santiagomartineztuta@gmail.com>
2026-06-09 18:15:48 -0700

    First fish added
    A fish and script added

---
+++ FinalProjectVGP236/Assets/Scripts/Fish.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    [Header("Fish Stats")]
+    public string fishName = "Basic Fish";
+    public int value = 10;               // Money 
+    public float swimSpeed = 2f;        
+    public float wiggleAmplitude = 0.2f; // Movement
+    public float wiggleFrequency = 2f;   // Speed
+
+    private Vector3 startPos;
+    private float timeOffset;
+
+    void Start()
+    {
+        startPos = transform.position;
+        timeOffset = Random.Range(0f, 10f); 
+    }
+    void Update()
+    {
+        Swim();
+    }
+    void Swim()
+    {
+        transform.Translate(Vector2.right * swimSpeed * Time.deltaTime);
+
+        float yOffset = Mathf.Sin((Time.time + timeOffset) * wiggleFrequency) * wiggleAmplitude;
+        transform.position = new Vector3(transform.position.x, startPos.y + yOffset, transform.position.z);
+    }
+
+    public int CatchFish()
+    {
+        Destroy(gameObject);
+        return value;
+    }
+}
